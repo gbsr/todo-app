61bd41df111cca0562000df679dcd1739775da11
Anders Hofsten <anders.jakobsen.hofsten@gmail.com>
2023-12-30 21:21:08 +0100

    fixed basic setup for touch devices.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
+let isTouchDevice = false;
 const element = document.getElementById('notes');
 
 text = 'this is my new note';
 let note = '';
-
-// add event listener for keydown
-handleNewNoteDialog();
-
 let notes = checkIfNotesExist();
 
+isTouchDevice = checkIfTouchDevice();
+handleNewNoteDialog();
 displayAllNotes(notes);
 
+function checkIfTouchDevice() {
+	isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+	if (isTouchDevice) {
+		document.querySelector('.container p:nth-child(1)').innerHTML =
+			"Tap to <span class='highlight create'>'create'</span> a task.";
+
+		document.querySelector('.container p:nth-child(2)').innerHTML =
+			"Swipe <span class='highlight delete'>'right'</span> to delete a task.";
+
+		document.querySelector('.container p.end').innerHTML =
+			"Swipe <span class='highlight complete'>'left'</span> to complete a task.";
+
+		document.body.addEventListener('touchstart', function (event) {
+			event.preventDefault();
+			console.log('touched');
+		});
+	}
+
+	console.log('touch is ' + isTouchDevice);
+	return isTouchDevice;
+}
 
 function displayAllNotes(notes) {
+
 	notes.forEach((note, index) => {
 		let text;
 		let completed;
@@ -49,15 +71,27 @@ function handleNewNoteDialog() {
 			if (document.activeElement === document.getElementById('noteInput')) {
 				return;
 			}
+		}
+	});
+	console.log('isTouchDevice: ' + isTouchDevice);
+	if (isTouchDevice) {
+		let container = document.querySelector('.container');
+		document.body.addEventListener('', function (event) {
+			event.preventDefault();
+			console.log('touched');
 			let noteDialog = document.getElementById('noteDialog');
-			noteDialog.showModal();
+
+			if (!noteDialog.open) {
+
+				noteDialog.showModal();
+			}
 
 			// hack to clear the input field, without this, the input field will have the previous value
 			setTimeout(function () {
 				document.getElementById('noteInput').value = '';
 			}, 0);
-		}
-	});
+		});
+	}
 
 	document.getElementById('noteDialog').addEventListener('submit', function (event) {
 		event.preventDefault();
@@ -72,7 +106,6 @@ function handleNewNoteDialog() {
 		this.close();
 	});
 }
-
 function addNewNote(text, index, completed) {
 
 	element.style.opacity = 1;
@@ -81,6 +114,7 @@ function addNewNote(text, index, completed) {
 	noteContainer.style.display = 'flex';
 	noteContainer.style.alignItems = 'center';
 	noteContainer.style.gap = '1rem';
+
 
 	if (completed) {
 		noteContainer.classList.add('strike-through');
@@ -91,6 +125,7 @@ function addNewNote(text, index, completed) {
 
 	element.appendChild(noteContainer);
 }
+
 function handleCompletedDialog() {
 	document.addEventListener('keydown', function (event) {
 		if (event.key.toLowerCase() === 'c') {
@@ -115,9 +150,39 @@ function handleCompletedDialog() {
 
 function completeTask(noteNumber) {
 	let notes = JSON.parse(localStorage.getItem('notes'));
+
+	if (isTouchDevice) {
+		let noteContainers = document.querySelectorAll('.noteContainer');
+
+		noteContainers.forEach((noteContainer, index) => {
+			let touchstartX = 0;
+			let touchendX = 0;
+
+			noteContainer.addEventListener('touchstart', function (event) {
+				touchstartX = event.changedTouches[0].screenX;
+			}, false);
+
+			noteContainer.addEventListener('touchend', function (event) {
+				touchendX = event.changedTouches[0].screenX;
+				handleSwipe();
+			}, false);
+
+			function handleSwipe() {
+				if (touchendX < touchstartX) {
+					deleteNote(index);
+				}
+			}
+		});
+	}
+
 	notes[noteNumber].completed = true;
 	localStorage.setItem('notes', JSON.stringify(notes));
 	location.reload();
+}
+
+function deleteNote(noteNumber) {
+	notes.splice(noteNumber, 1);
+	displayNotes();
 }
 
 function handleDeleteNoteDialog() {
